714f3d38f582c62c142e4b4961ebcd3179966e5e
PrimmR <PrimmR@icloud.com>
2023-07-30 12:58:45 +0100

    Fixed more display issues, changed constants

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@ const HEIGHT: i32 = 1080;
 const WIDTH: i32 = HEIGHT * 16/9;
 
 // Percentages
-const TITLE_SIZE: f32 = 6.75;
-const CAPTION_SIZE: f32 =  10.8;
-const SPACING_FROM_MID: f32 = 10.8;
+const TITLE_SIZE: f32 = 0.125;
+const CAPTION_SIZE: f32 =  0.08;
+const SPACING_FROM_MID: f32 = 0.065;
 const MARGIN: f32 = 0.8;
-const LINE_BREAK: f32 = 0.0125;
+const LINE_BREAK: f32 = 0.04;
 const MAX_LINES: i32 = 4;
-
 const WIDE_TEXT: bool = true;
+
+const OUTPUT_FILE: &str = "output.png";
 
 fn center(constraint: i32, size: i32) -> i32 {
     (constraint - size) / 2
@@ -23,12 +24,12 @@ fn center(constraint: i32, size: i32) -> i32 {
 
 fn draw_title(img: &mut RgbImage, font: &Font<'_>) {
     let text = "N O  M O R E";
-    let factor = HEIGHT as f32 / TITLE_SIZE;
+    let factor = HEIGHT as f32 * TITLE_SIZE;
     let scale = Scale{x: factor, y: factor};
     let size = text_size(scale, font, text);
     let x = center(i32::try_from(WIDTH).unwrap(), size.0);
     let y = center(i32::try_from(HEIGHT).unwrap(), size.1);
-    let y = y - (HEIGHT as f32 / SPACING_FROM_MID) as i32;
+    let y = y - (HEIGHT as f32 * SPACING_FROM_MID) as i32;
 
     draw_text_mut(img, Rgb([255, 255, 255]), x, y, scale, font, text);
 }
@@ -36,24 +37,15 @@ fn draw_title(img: &mut RgbImage, font: &Font<'_>) {
 fn draw_text(img: &mut RgbImage, font: &Font<'_>, text: Vec<String>) {
     let mut i = 0;
     for line in &text {
-        let factor = HEIGHT as f32 / CAPTION_SIZE;
+        let factor = HEIGHT as f32 * CAPTION_SIZE;
         let scale = Scale{x: factor, y: factor};
         let size = text_size(scale, font, line);
         let x = center(WIDTH, size.0);
         let y = center(HEIGHT, size.1);
-        let y = y + (HEIGHT as f32 / SPACING_FROM_MID) as i32 + (size.1 + (HEIGHT as f32 * LINE_BREAK) as i32) * i;
+        let y = y + (HEIGHT as f32 * SPACING_FROM_MID) as i32 + (size.1 + (HEIGHT as f32 * LINE_BREAK) as i32) * i;
 
         draw_text_mut(img, Rgb([255, 255, 255]), x, y, scale, font, line);
         i += 1;
-    }
-}
-
-// Divides and floors floats to integers, floors integers down by 1
-fn div_floor(num: i32, den: i32) -> i32 {
-    if num % den == 0 {
-        (num / den) - 1
-    } else {
-        num / den
     }
 }
 
@@ -71,7 +63,7 @@ fn split_text(text: String, font: &Font<'_>) -> Result<Vec<String>, String> {
         (text, " ")
     };
 
-    let factor = HEIGHT as f32 / CAPTION_SIZE;
+    let factor = HEIGHT as f32 * CAPTION_SIZE;
     let scale = Scale{x: factor, y: factor};
     let size = text_size(scale, font, &text);
 
@@ -86,11 +78,18 @@ fn split_text(text: String, font: &Font<'_>) -> Result<Vec<String>, String> {
 
     let mut lines = vec![String::from(""); num_lines as usize];
     for i in 0..words.len() {
-        if text_size(scale, font, words[i]).0 > LIMIT {
+        let word_width = text_size(scale, font, words[i]).0;
+        let acc_width = text_size(scale, font, words[..i+1].join(" ").as_str()).0;
+
+        if word_width > LIMIT {
             return Err(String::from("Your input contains a word that is too long"));
         }
-        let acc_width = text_size(scale, font, words[..i+1].join(" ").as_str()).0;
-        let line = div_floor(acc_width, target);
+        let line = if word_width < target {
+            acc_width / target
+        }
+        else {
+            acc_width / LIMIT
+        };
         lines[line as usize] += &format!("{} ", words[i]);
     }
 
@@ -104,15 +103,12 @@ fn main() {
 
     image::imageops::vertical_gradient(&mut img, &start, &end);
 
-    let font = Vec::from(include_bytes!("FMedium.ttf") as &[u8]);
+    let font = Vec::from(include_bytes!("FLight.ttf") as &[u8]);
     let font = Font::try_from_vec(font).unwrap();
 
     let text;
     loop {
         println!("Category:");
-        // let cat = String::from("Shorties");
-        // let cat = String::from("This is a very long string nooo its run over");
-        // let cat = String::from("This is a very long string and now it's even longer and now it's even longer");
 
         let mut cat = String::new();
         io::stdin().read_line(&mut cat).expect("failed to readline");
@@ -129,6 +125,6 @@ fn main() {
     draw_title(&mut img, &font);
     draw_text(&mut img, &font, text);
 
-    img.save("output.png").unwrap();
+    img.save(OUTPUT_FILE).unwrap();
     println!("Written to output.png")
 }
